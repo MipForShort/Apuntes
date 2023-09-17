3b5985e848bf3460c09d409de4602a81928dd24c
Mip <mariomip98@gmail.com>
2023-09-16 19:35:42 -0600

    Añadiendo inputs

---
+++ Python/Curso de Python DALTO/Inputs/input_numeros.py
@@ -0,0 +1,9 @@
+#Pedimos un número al usuario
+numero = input("Dame un número: ")
+
+#Convierto el número a entero y lo multiplica por 2
+resultado = int(numero) * 2
+print(resultado)
+#Convierto el número a float y lo multiplica por 2
+resultado = float(numero) * 2
+print(resultado)
