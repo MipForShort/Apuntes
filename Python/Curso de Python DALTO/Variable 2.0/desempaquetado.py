a0b6843c4891263803fabcd0f85f98cb459ddd33
Mip <mariomip98@gmail.com>
2023-09-17 18:56:27 -0600

    Añadiendo carpeta Variable 2.0

---
+++ Python/Curso de Python DALTO/Variable 2.0/desempaquetado.py
@@ -0,0 +1,8 @@
+datos_en_tupla = ("Lucas", "Dalto", 1000000)
+datos_en_lista = ["Lucas", "Dalto", 1000000]
+
+nombre, apellido, susbs = datos_en_tupla
+print(datos_en_tupla)
+
+nombre, apellido, susbs = datos_en_lista
+print(datos_en_lista)
---
+++ Python/Curso de Python DALTO/Variable 2.0/tuplas.py
@@ -0,0 +1,13 @@
+#La tupla puede ser una función en base a los datos que le demos a empaquetar
+tupla = tuple(["dato1", "dato2"])
+print(tupla)
+
+#Una forma de crear tuplas es escribiendo el nombre de la variable para después
+#mediante comillas asignar datos que se separan mediante una coma
+tupla = "dato1.5", "dato2.5"
+print(tupla)
+
+#Creando una tupla sin paréntesis de un sólo dato
+#Observa que se define poniendo una coma al final del único dato
+tupla = "dato1.5.1",
+print(type(tupla), "\n", tupla)
